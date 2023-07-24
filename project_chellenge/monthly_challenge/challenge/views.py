b8b05d7bdc76528310237bc3070da2ee93ddcd42
Daniel-bee <daniel123_86@live.com>
2023-07-24 20:58:51 +0300

    template render

--- project_chellenge/monthly_challenge/challenge/views.py
+++ project_chellenge/monthly_challenge/challenge/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader  import render_to_string
 # Create your views here.
 challanges = {
     "january": "Eat no meat for the entire month!",
     "february": "Walk everyday at least 20 minute every day!",
     "march": "Learn Django for at least 20 minutes every day!"
 }
-def january(request):
-    return HttpResponse('Eat no meat for the entire month!')
-
-def february(request):
-    return HttpResponse('Walk everyday at least 20 minute!')
-
+def index(request):
+    month_link = ""
+    months = list(challanges.keys())
+    for month in months:
+        dir_ = reverse('monthly-challenge', args=[month])
+        month_link += f"<li><a href='{dir_}'>{month.capitalize()}</a></li>"
+    path = f"<ul>{month_link}</ul>"
+    return HttpResponse(path)
 def monthly_challenges_by_number(request, month):
     months = list(challanges.keys())
    
@@ -25,7 +28,7 @@ def monthly_challenges_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge = challanges[month]
+        return render(request, "challenge/challenge.html", {"text":challenge, "title": month})
     except:
         return HttpResponseNotFound("Month not supported!")
     
-    return HttpResponse(challenge)
--- project_chellenge/monthly_challenge/monthly_challenge/urls.py
+++ project_chellenge/monthly_challenge/monthly_challenge/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("challenge/", include("challenge.urls"))
+    path("challenges/", include("challenge.urls"))
 ]
